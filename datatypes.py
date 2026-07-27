b5d53c0181e9b1ab2d2130ef177872021d7bfc75
Shagun Punia <shagun.punia.mca.2025@miet.ac.in>
2026-07-27 11:07:34 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+food='samosa'
+age=7
+area=6.98
+
+print(type(area))
+print("age has data type:",type(age))
+print('------------------------------')
+
+#program to print age an its data type
+print('program to print age and its data type')
+age=input('enter your age:')
+print('your age is :',age)
+print('Data type of age is:',type(age))
+print('-------------------------------')
+
